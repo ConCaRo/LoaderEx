ae27de534dea7b6d043d2296ef396e2c046df0b4
TanTrong <trongnt1990@gmail.com>
2016-10-31 13:34:15 +0700

    Init example of Loader(Cursor) and AsynctaskLoader

---
+++ app/src/main/java/co/concaro/loader/LoaderContactActivity.java
@@ -0,0 +1,143 @@
+package co.concaro.loader;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+/**
+ * Leaked from https://developer.android.com/guide/components/loaders.html#summary 31/10/2016
+ */
+
+public class LoaderContactActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>, SearchView.OnQueryTextListener, AdapterView.OnItemClickListener {
+
+    // This is the Adapter being used to display the list's data.
+    SimpleCursorAdapter mAdapter;
+
+    // If non-null, this is the current filter the user has provided.
+    String mCurFilter;
+
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+        // Give some text to display if there is no data.  In a real
+        // application this would come from a resource.
+
+        // We have a menu item to show in action bar.
+
+        // Create an empty adapter we will use to display the loaded data.
+        mAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, null,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.CONTACT_STATUS},
+                new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(this);
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
+        getLoaderManager().initLoader(0, null, this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item = menu.add("Search");
+        item.setIcon(android.R.drawable.ic_menu_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView sv = new SearchView(this);
+        sv.setOnQueryTextListener(this);
+        item.setActionView(sv);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public boolean onQueryTextChange(String newText) {
+        // Called when the action bar search text has changed.  Update
+        // the search filter, and restart the loader to do a new query
+        // with this filter.
+        mCurFilter = !TextUtils.isEmpty(newText) ? newText : null;
+        getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        // Don't care about this.
+        return true;
+    }
+
+    // These are the Contacts rows that we will retrieve.
+    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[]{
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.CONTACT_PRESENCE,
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.LOOKUP_KEY,
+    };
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.  This
+        // sample only has one Loader, so we don't care about the ID.
+        // First, pick the base URI to use depending on whether we are
+        // currently filtering.
+        Uri baseUri;
+        if (mCurFilter != null) {
+            baseUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI,
+                    Uri.encode(mCurFilter));
+        } else {
+            baseUri = ContactsContract.Contacts.CONTENT_URI;
+        }
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+        return new CursorLoader(this, baseUri,
+                CONTACTS_SUMMARY_PROJECTION, select, null,
+                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        mAdapter.swapCursor(null);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("FragmentComplexList", "Item clicked: " + id);
+    }
+
+}
